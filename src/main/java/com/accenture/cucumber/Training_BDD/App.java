1402f77b94b7c5c1982933a55ce720f16d23ec9f
COELF003 <fernanda.menks@accenture.com>
2016-07-31 21:06:47 -0400

    Initial commit

---
+++ src/main/java/com/accenture/cucumber/Training_BDD/App.java
@@ -0,0 +1,13 @@
+package com.accenture.cucumber.Training_BDD;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
