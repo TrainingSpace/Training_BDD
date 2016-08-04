de6367d83d08cc7783d097bfe85607bd505756a0
COELF003 <fernanda.menks@accenture.com>
2016-08-04 01:45:03 -0400

    Updates in Selenium implementation
    Updated step definition for AC001

--- src/test/java/com/accenture/cucumber/Training_BDD/AC001_SampleBanking_StepDefinition.java
+++ src/test/java/com/accenture/cucumber/Training_BDD/AC001_SampleBanking_StepDefinition.java
@@ -2,6 +2,7 @@ package com.accenture.cucumber.Training_BDD;
 
 import java.util.List;
 
+import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
@@ -13,7 +14,6 @@ import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-import org.junit.Assert;
 
 public class AC001_SampleBanking_StepDefinition {
 
