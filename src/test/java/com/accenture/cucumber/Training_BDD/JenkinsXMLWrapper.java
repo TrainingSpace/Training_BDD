cae7695338ae8bad4d399be8f80e1d8b155ee662
fmenks <fmenks@gmail.co>
2017-06-06 18:28:42 -0400

    Parsing through XML to identify lastBuild duration and status

---
+++ src/test/java/com/accenture/cucumber/Training_BDD/JenkinsXMLWrapper.java
@@ -0,0 +1,81 @@
+package com.accenture.cucumber.Training_BDD;
+
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.Element;
+import org.dom4j.io.SAXReader;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.LinkedList;
+import java.util.List;
+
+public class JenkinsXMLWrapper {
+
+    public static class XMLRoot {
+        List<JenkinsJob> jobs = new LinkedList<>();
+        Element something;
+
+        XMLRoot(String target, String sourceURL) {
+            try {
+                URL rootURL = new URL(sourceURL);
+                Document dom =  new SAXReader().read(rootURL);
+
+                switch (target){
+                    case "job":
+                        for( Element job : (List<Element>)dom.getRootElement().elements("job")) {
+                            JenkinsJob jenkinsJob = new JenkinsJob(job.elementText("name"), job.elementText("url"), job.elementText("name"),"","");
+                            jobs.add(jenkinsJob);
+                        }
+                        break;
+
+                    case "lastBuild":
+                        something = dom.getRootElement();
+                        System.out.println("duration = " + something.node(10).getText());
+                        System.out.println("\nresult = " + something.node(17).getText());
+                        break;
+                }
+
+            } catch (MalformedURLException | DocumentException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+        public class JenkinsJob {
+            String name;
+            String url;
+            String color;
+            String duration;
+            String result;
+            JenkinsJob(String jobName, String jobURL, String jobColor, String jobDuration, String jobResult) {
+                name = jobName;
+                url = jobURL;
+                color = jobColor;
+                duration = jobDuration;
+                result = jobResult;
+            }
+        }
+
+        public void ListAllJobs() {
+            for (int i = 0; i < jobs.size(); i++) {
+                System.out.println(String.format("Name: %s\tURL: %s\tStatus: %s", jobs.get(i).name, jobs.get(i).url, jobs.get(i).color));
+            }
+        }
+
+    }
+
+
+
+
+    public static void main(String[] args) throws Exception {
+        // every Hudson model object exposes the .../api/xml, but in this example
+        // we'll just take the root object as an example
+        XMLRoot root;
+        root = new XMLRoot("lastBuild","http://jenkins-tcoe-qa.disney.com/job/Selenium_ALM_Sync/lastBuild/api/xml");
+        // scan through the job list and print its data
+       // root.ListAllJobs();
+
+    }
+}
+
